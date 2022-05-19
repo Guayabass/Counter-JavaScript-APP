d066429f611f8117e26c386514793c44f9eebd75
Guayabas <guayabas0399@gmail.com>
2022-05-19 15:29:53 -0600

    finished app

--- index.js
+++ index.js
@@ -3,10 +3,34 @@
 let count = 0
 
 function increment(){
-    count = count + 1
+    count += 1
     document.getElementById("counter").innerText = count //sets the number to 5
 }
 
 function save(){
+    let entries = document.getElementById("entries")
+    let savedValue = count
+    let dash = " - "
 
+    entries.innerText += " " + savedValue + dash
 }
+
+function deleteNumber(){
+    count = 0
+    document.getElementById("counter").innerText = count
+}
+
+/*let welcomeEl = document.getElementById("welcome-el")
+
+let name = "Per Harald Borgen"
+let greeting = "Welcome back "
+
+welcomeEl.innerText = greeting + name
+
+// Add an emoji to the end! 👋
+// WRITE YOUR CODE BELOW HERE
+// HINT: count = count + 1
+
+welcomeEl.innerText += "👋"
+el += es como para hacer count = count + 1
+*/
